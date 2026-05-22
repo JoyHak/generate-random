f10edb00b95def9f98bf766a0c4ee117312a9c2e
JoyHak <access-restricted@onionmail.org>
2026-05-22 16:16:52 +0300

    Update concepts

--- generate.hpp
+++ generate.hpp
@@ -26,30 +26,46 @@
 #ifndef GENERATE_HPP_
 #define GENERATE_HPP_
 #include <random>
-#include <type_traits>
+#include <concepts>
+
+/**
+ * According to the <a href="https://eel.is/c++draft/rand#req.genl-1">standart</a>:
+ * <br><i>where the template parameters are not constrained, the names of template parameters
+ * are used to express type requirements on an instantiated template T</i>.<br><br>
+ *
+ * The standard does not describe <i>concepts</i> that would allow efficient type deduction for wrappers.
+ * So we will have to create our own <i>concepts</i> that use internal functions from
+ * <a href="https://github.com/llvm/llvm-project/blob/8cc944cb29cec7974509a34ae8ad85c483cbd120/libcxx/include/__random/is_valid.h#L47">LLVM Clang</a>.
+ * They will help the compiler choose template specifications that allow generating
+ * containers of any type: `char, bool, string`, ... despite the restrictions of distributions.
+ */
 
 template<typename T>
-concept Arithmetic = std::is_arithmetic_v<T> && !std::is_same_v<T, bool>;
+concept Integer = std::__libcpp_random_is_valid_inttype<T>::value;
 
 template<typename T>
-concept Integer = std::is_integral_v<T> && !std::is_same_v<T, bool>;
+concept Floating = std::__libcpp_random_is_valid_realtype<T>::value;
 
 template<typename T>
-concept Floating = std::is_floating_point_v<T>;
+concept Arithmetic = Integer<T> || Floating<T>;
 
-template<typename Container>
-concept ContainerPush = requires(Container c, typename Container::value_type v) {
+template<typename Cont>
+concept ContainerPush = requires(Cont c, typename Cont::value_type v) {
     c.push(v);
 };
 
-template<typename Container>
-concept ContainerPushBack = requires(Container c, typename Container::value_type v) {
+template<typename Cont>
+concept ContainerPushBack = requires(Cont c, typename Cont::value_type v) {
     c.push_back(v);
 };
 
-template<typename Container>
-concept ContainerInteger = Integer<typename Container::value_type>;
+template<typename Cont>
+concept ContainerInteger = Integer<typename Cont::value_type>;
 
+template<typename Gen>
+concept UniformGenerator = std::uniform_random_bit_generator<Gen>;
+
+// ── Distributions ────────────────────────────────────────────────────────────────────
 
 template<Integer Item>
 inline auto make_distribution(Item min, Item max) {
@@ -62,23 +78,27 @@ inline auto make_distribution(Item min, Item max) {
 }
 
 inline auto make_distribution() {
+    // 50% true, 50% false
     return std::bernoulli_distribution(0.5);
 }
 
-template<ContainerPush Container, typename Distribution, typename Generator>
+// ── Fillers ──────────────────────────────────────────────────────────────────────────
+
+template<ContainerPush Container, typename Distribution, UniformGenerator Generator>
 inline void fill_container(Container& container, size_t count, Distribution& dist, Generator& gen) {
     for (size_t i = 0; i < count; ++i) {
         container.push(dist(gen));
     }
 }
 
-template<ContainerPushBack Container, typename Distribution, typename Generator>
+template<ContainerPushBack Container, typename Distribution, UniformGenerator Generator>
 inline void fill_container(Container& container, size_t count, Distribution& dist, Generator& gen) {
     for (size_t i = 0; i < count; ++i) {
         container.push_back(dist(gen));
     }
 }
 
+// ── Generators ───────────────────────────────────────────────────────────────────────
 
 /**
  * @brief Generates a container of random values with the given distribution and generator.
@@ -86,6 +106,7 @@ inline void fill_container(Container& container, size_t count, Distribution& dis
  * @tparam Container The container type.
  * @tparam Item Container item type (auto-inferred).
  * @tparam Generator Random engine for sequence generation.
+ * @tparam Seed Unsigned integer type.
  * @param count Number of elements.
  * @param min Minimum value.
  * @param max Maximum value.
@@ -95,12 +116,13 @@ inline void fill_container(Container& container, size_t count, Distribution& dis
 template<
     typename Container,
     Arithmetic Item = typename Container::value_type,
-    std::uniform_random_bit_generator Generator = std::mt19937
+    UniformGenerator Generator = std::mt19937,
+    typename Seed = unsigned int
 >
 Container generate(
     size_t count = 10,
     Item min = 1, Item max = 26,
-    std::random_device::result_type seed = std::random_device{}()
+    Seed seed = std::random_device{}()
 ) {
     Container container;
 
@@ -120,17 +142,19 @@ Container generate(
  * @note The default distribution does not support the `bool` type, so this template uses integral type.
  * @tparam Container The container type.
  * @tparam Generator Random engine for sequence generation.
+ * @tparam Seed Unsigned integer type.
  * @param count Number of elements.
  * @param seed Initial seed value for the random number engine. Using the same seed allows reproducible results.
  * @return The filled container.
  */
 template<
     typename Container,
-    std::uniform_random_bit_generator Generator = std::mt19937
+    UniformGenerator Generator = std::mt19937,
+    typename Seed = unsigned int
 >
     Container generate_bool(
     size_t count = 10,
-    std::random_device::result_type seed = std::random_device{}()
+    Seed seed = std::random_device{}()
 ) {
     Container container;
 
